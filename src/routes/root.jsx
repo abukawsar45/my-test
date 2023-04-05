04ee1c169bb7cd74f35731328e479743efb01920
abukawsar45 <kawsar1146236@gmail.com>
2023-04-05 19:23:13 +0600

    first commit

---
+++ src/routes/root.jsx
@@ -0,0 +1,56 @@
+import { Link, Outlet } from "react-router-dom";
+
+export default function Root() {
+  return (
+    <>
+      <div id="sidebar">
+        <h1>React Router Contacts</h1>
+          <nav>
+            <li>
+              <Link to='contacts/1'> Your name </Link>
+          </li>
+          <li>
+            <Link to={'contacts/2'}> Your friend</Link>
+          </li>
+          </nav>
+         
+        <div>
+          <form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div
+              id="search-spinner"
+              aria-hidden
+              hidden={true}
+            />
+            <div
+              className="sr-only"
+              aria-live="polite"
+            ></div>
+          </form>
+          <form method="post">
+            <button type="submit">New</button>
+          </form>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href={`/contacts/1`}>Your Name</a>
+            </li>
+            <li>
+              <a href={`/contacts/2`}>Your Friend</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="detail">
+        <Outlet/>
+      </div>
+    </>
+  );
+}
